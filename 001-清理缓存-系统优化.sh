7c3c4d711de56aefa57c81d32480a039eb73f1ae
thehejian <745544752@qq.com>
2021-12-05 11:44:51 +0800

    Create 001-清理缓存-系统优化.sh

---
+++ 001-清理缓存-系统优化.sh
@@ -0,0 +1,35 @@
+#删除临时文件
+  #win键，搜索磁盘清理，选择程序-磁盘清理
+  #选择c盘
+  #选择全部
+
+#微软商店缓存
+  #win 搜CMD
+  #WSreset.exe
+  #或者 win+R WSreset.exe
+#DNS缓存
+  #win+R搜索cmd，管理员方式运行
+      #ipconfig /flushDNS
+      #刷新DNS缓存
+  #修改被劫持的域名
+      #C盘   Windows   System32    drivers   etc   hosts
+      #属性   安全  更改权限
+#浏览器缓存清理
+    #ctrl + shift + del
+#文件资源管理器的缓存
+    #win搜索 文件资源管理器选项
+    #常规   隐私    （清除）
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
